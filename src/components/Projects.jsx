3fb533146eacf9502b5da8e38405816d3ef93a53
amandasuzzanne <amandasuzanne12@gmail.com>
2024-09-19 15:18:36 +0300

    Add more content for project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,21 @@ function Projects() {
   return (
     <div className='projects'>
         <h2>Cool ideas that I have worked on</h2>
+        <div className='project-container'>
+            <div className='project'>
+                <h3>Project 1</h3>
+                <p>Description of project 1</p>
+            </div>
+            <div className='project'>
+                <h3>Project 1</h3>
+                <p>Description of project 1</p>
+            </div>
+            <div className='project'>
+                <h3>Project 1</h3>
+                <p>Description of project 1</p>
+            </div>
+        </div>
+
     </div>
   )
 }
