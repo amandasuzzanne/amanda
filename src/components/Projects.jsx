2dbace675a1ab8797cd244b2b1a6194e4d348d8f
amandasuzzanne <amandasuzanne12@gmail.com>
2024-09-19 14:47:27 +0300

    Add Projects.jsx component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Projects.css';
+
+function Projects() {
+  return (
+    <div className='projects'>
+        <h2>Cool ideas that I have worked on</h2>
+    </div>
+  )
+}
+
+export default Projects
